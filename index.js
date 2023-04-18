b3c66d58afe58ebe16c5d3f2054f8c2fe65c7461
sayka11 <tvaradze.suraya@mail.ru>
2023-04-18 12:51:41 +0400

    last

---
+++ index.js
@@ -0,0 +1,133 @@
+let a = {
+    "2":"twenty",
+    "3": "thirty",
+    "4": "forty",
+    "5": "fifty",
+    "6": "sixty",
+    "7": "seventy",
+    "8": "eighty",
+    "9": "ninety"
+}
+let b = {
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine"
+};
+let number = prompt("Please enter the two-digits number");
+function result(){
+
+        if (number == 10 ) {alert("ten")};
+        if (number == 11 ) {alert("eleven")};
+        if (number == 12 ) {alert("twelve")};
+        if (number == 13 ) {alert("thirteen")};
+        if (number == 14 ) {alert("fourteen")};
+        if (number == 15 ) {alert("fifteen ")};
+        if (number == 16 ) {alert("sixteen ")};
+        if (number == 17 ) {alert("seventeen ")};
+        if (number == 18 ) {alert("eighteen ")};
+        if (number == 19 ) {alert("nineteen ")};
+
+ if(number === 20){alert(a["2"])};
+ if(number === 30){alert(a["3"])};
+ if(number === 40){alert(a["4"])};
+ if(number === 50){alert(a["5"])};
+ if(number === 60){alert(a["6"])};
+ if(number === 70){alert(a["7"])};
+ if(number === 80){alert(a["8"])};
+ if(number === 90){alert(a["3"])};
+
+ if(number[0]==2){
+ if(number[1]== 1){alert(a["2"] + " " + b["1"])}
+ if(number[1]== 2){alert(a["2"] + " " + b["2"])}
+ if(number[1]== 3){alert(a["2"] + " " + b["3"])}
+ if(number[1]== 4){alert(a["2"] + " " + b["4"])}
+ if(number[1]== 5){alert(a["2"] + " " + b["5"])}
+ if(number[1]== 6){alert(a["2"] + " " + b["6"])}
+ if(number[1]== 7){alert(a["2"] + " " + b["7"])}
+ if(number[1]== 8){alert(a["2"] + " " + b["8"])}
+ if(number[1]== 9){alert(a["2"] + " " + b["9"])}}
+
+ if(number[0]==3){
+ if(number[1]== 1){alert(a["3"] + " " + b["1"])}
+ if(number[1]== 2){alert(a["3"] + " " + b["2"])}
+ if(number[1]== 3){alert(a["3"] + " " + b["3"])}
+ if(number[1]== 4){alert(a["3"] + " " + b["4"])}
+ if(number[1]== 5){alert(a["3"] + " " + b["5"])}
+ if(number[1]== 6){alert(a["3"] + " " + b["6"])}
+ if(number[1]== 7){alert(a["3"] + " " + b["7"])}
+ if(number[1]== 8){alert(a["3"] + " " + b["8"])}
+if(number[1]== 9){alert(a["3"] + " " + b["9"])}}
+
+    if(number[0]==4){
+ if(number[1]== 1){alert(a["4"] + " " + b["1"])}
+ if(number[1]== 2){alert(a["4"] + " " + b["2"])}
+ if(number[1]== 3){alert(a["4"] + " " + b["3"])}
+ if(number[1]== 4){alert(a["4"] + " " + b["4"])}
+ if(number[1]== 5){alert(a["4"] + " " + b["5"])}
+ if(number[1]== 6){alert(a["4"] + " " + b["6"])}
+ if(number[1]== 7){alert(a["4"] + " " + b["7"])}
+ if(number[1]== 8){alert(a["4"] + " " + b["8"])}
+ if(number[1]== 9){alert(a["4"] + " " + b["9"])}}
+
+ if(number[0]==5){
+ if(number[1]== 1){alert(a["5"] + " " + b["1"])}
+ if(number[1]== 2){alert(a["5"] + " " + b["2"])}
+ if(number[1]== 3){alert(a["5"] + " " + b["3"])}
+ if(number[1]== 4){alert(a["5"] + " " + b["4"])}
+ if(number[1]== 5){alert(a["5"] + " " + b["5"])}
+ if(number[1]== 6){alert(a["5"] + " " + b["6"])}
+ if(number[1]== 7){alert(a["5"] + " " + b["7"])}
+ if(number[1]== 8){alert(a["5"] + " " + b["8"])}
+ if(number[1]== 9){alert(a["5"] + " " + b["9"])}}
+ 
+ if(number[0]==6){
+ if(number[1]== 1){alert(a["6"] + " " + b["1"])}
+ if(number[1]== 2){alert(a["6"] + " " + b["2"])}
+ if(number[1]== 3){alert(a["6"] + " " + b["3"])}
+ if(number[1]== 4){alert(a["6"] + " " + b["4"])}
+ if(number[1]== 5){alert(a["6"] + " " + b["5"])}
+ if(number[1]== 6){alert(a["6"] + " " + b["6"])}
+ if(number[1]== 7){alert(a["6"] + " " + b["7"])}
+ if(number[1]== 8){alert(a["6"] + " " + b["8"])}
+ if(number[1]== 9){alert(a["6"] + " " + b["9"])}}
+
+ if(number[0]==7){
+ if(number[1]== 1){alert(a["7"] + " " + b["1"])}
+ if(number[1]== 2){alert(a["7"] + " " + b["2"])}
+ if(number[1]== 3){alert(a["7"] + " " + b["3"])}
+ if(number[1]== 4){alert(a["7"] + " " + b["4"])}
+ if(number[1]== 5){alert(a["7"] + " " + b["5"])}
+ if(number[1]== 6){alert(a["7"] + " " + b["6"])}
+ if(number[1]== 7){alert(a["7"] + " " + b["7"])}
+ if(number[1]== 8){alert(a["7"] + " " + b["8"])}
+ if(number[1]== 9){alert(a["7"] + " " + b["9"])}}
+
+ if(number[0]==8){
+ if(number[1]== 1){alert(a["8"] + " " + b["1"])}
+ if(number[1]== 2){alert(a["8"] + " " + b["2"])}
+ if(number[1]== 3){alert(a["8"] + " " + b["3"])}
+ if(number[1]== 4){alert(a["8"] + " " + b["4"])}
+ if(number[1]== 5){alert(a["8"] + " " + b["5"])}
+ if(number[1]== 6){alert(a["8"] + " " + b["6"])}
+ if(number[1]== 7){alert(a["8"] + " " + b["7"])}
+ if(number[1]== 8){alert(a["8"] + " " + b["8"])}
+ if(number[1]== 9){alert(a["8"] + " " + b["9"])}}
+
+ if(number[0]==9){
+ if(number[1]== 1){alert(a["9"] + " " + b["1"])}
+ if(number[1]== 2){alert(a["9"] + " " + b["2"])}
+ if(number[1]== 3){alert(a["9"] + " " + b["3"])}
+ if(number[1]== 4){alert(a["9"] + " " + b["4"])}
+ if(number[1]== 5){alert(a["9"] + " " + b["5"])}
+ if(number[1]== 6){alert(a["9"] + " " + b["6"])}
+ if(number[1]== 7){alert(a["9"] + " " + b["7"])}
+ if(number[1]== 8){alert(a["9"] + " " + b["8"])}
+ if(number[1]== 9){alert(a["9"] + " " + b["9"])}}
+}
+result();
